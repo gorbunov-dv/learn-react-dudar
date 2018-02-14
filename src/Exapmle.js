0c0d61de99905d61a114cbb54af63c981317a60c
Dmitry <gorbunov.webdev@gmail.com>
2018-02-14 23:40:11 +0400

    Проверка на практике добавления инофрмации в другой блок

---
+++ src/Exapmle.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Example extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Привет</h3>
+            </div>    
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Example from './Exapmle';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Example />, document.getElementById('examle'));
 registerServiceWorker();
