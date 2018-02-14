e69c62161c2c4b71882929ccf4339b859f4e1bc9
Dmitry <gorbunov.webdev@gmail.com>
2018-02-15 00:24:48 +0400

    Удалось разобраться составными компонетами

---
+++ src/Example.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Example extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Example</h1>
+      </div>
+    );
+  }
+}
+
+export default Example;
